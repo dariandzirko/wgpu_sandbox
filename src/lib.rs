785340f248ca2c6f4ddb735f74d0ca9aefbda998
Darian <dariandzirko@gmail.com>
2023-06-04 14:30:14 -0600

    Finished part 2 plus challenge

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
+    clear_color: wgpu::Color,
 }
 
 impl State {
@@ -84,6 +85,8 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let clear_color = wgpu::Color::BLACK;
+
         Self {
             surface,
             device,
@@ -91,6 +94,7 @@ impl State {
             config,
             size,
             window,
+            clear_color,
         }
     }
 
@@ -107,9 +111,19 @@ impl State {
         }
     }
 
-    #[allow(unused_variables)]
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                self.clear_color = wgpu::Color {
+                    r: position.x as f64 / self.size.width as f64,
+                    g: position.y as f64 / self.size.height as f64,
+                    b: 1.0,
+                    a: 1.0,
+                };
+                true
+            }
+            _ => false,
+        }
     }
 
     fn update(&mut self) {
@@ -136,12 +150,7 @@ impl State {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
+                        load: wgpu::LoadOp::Clear(self.clear_color),
                         store: true,
                     },
                 })],
