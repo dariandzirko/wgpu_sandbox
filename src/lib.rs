ec0defc22947dd6ee60462295864bf672ef067a4
Darian <dariandzirko@gmail.com>
2023-06-06 08:06:23 -0600

    There is a magenta pentagon

--- src/lib.rs
+++ src/lib.rs
@@ -17,23 +17,31 @@ struct Vertex {
     color: [f32; 3],
 }
 
-// lib.rs
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, //A
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, //B
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, //C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, //D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, //E
 ];
 
-// lib.rs
+const INDECES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+
 impl Vertex {
     fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
@@ -66,7 +74,8 @@ struct State {
     render_pipeline: wgpu::RenderPipeline,
 
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indeces: u32,
 }
 
 impl State {
@@ -196,7 +205,13 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
-        let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDECES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        let num_indeces = INDECES.len() as u32;
 
         Self {
             surface,
@@ -207,7 +222,8 @@ impl State {
             window,
             render_pipeline,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indeces,
         }
     }
 
@@ -265,9 +281,9 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indeces, 0, 0..1);
         }
 
         // Submit will accept anything that implements IntoIter
